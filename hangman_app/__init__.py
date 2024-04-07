1a3914e55d8a851da1b90ca51ba7b00b8c561b44
Mykoliukass <146163704+Mykoliukass@users.noreply.github.com>
2024-04-07 20:19:47 +0300

    Add a whole new structure and start from zero again

--- hangman_app/__init__.py
+++ hangman_app/__init__.py
@@ -1,27 +1,41 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from os import path
 from flask_login import LoginManager
-from flask_mail import Mail
 
-app = Flask(__name__, template_folder="templates")
-app.config["DEBUG"] = True
-app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hangman_users.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "your_email@gmail.com"  # Provide your email
-app.config["MAIL_PASSWORD"] = "your_email_password"  # Provide your email password
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "signin"
-login_manager.login_message_category = "info"
 
-mail = Mail(app)
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "hjshjhdjah kjshkjdhjs"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
-from hangman_app import routes  # Import routes at the end to avoid circular imports
+    from .routes import routes
+    from .auth import auth
+
+    app.register_blueprint(routes, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User, Game
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from hangman_app import app
+from hangman_app import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
